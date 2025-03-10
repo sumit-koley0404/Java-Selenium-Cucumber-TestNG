01b330dc4d48b068409a4d52298c9b1fc631f588
sumit-koley0404 <sumitkoley180@gmail.com>
2025-03-10 22:27:08 +0530

    initial Commit

---
+++ java-selenium-bdd-testNG/src/main/java/BDDCucumberAutomation/java_selenium_bdd_testNG/App.java
@@ -0,0 +1,13 @@
+package BDDCucumberAutomation.java_selenium_bdd_testNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
